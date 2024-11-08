7ef3b6b0ca0069c8ac041609a8d6d13bf148fca3
Divishayadav <yadavdivisha9@gmail.com>
2024-11-08 19:42:32 +0530

    Update PhoneNumberGuesserGUI.java

--- PhoneNumberGuesserGUI.java
+++ PhoneNumberGuesserGUI.java
@@ -1,17 +1,16 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Map;
 
-public class PhoneNumberGuesser1 {
+public class PhoneNumberGuesser {
 
     private static Map<String, String> countryCodes = new HashMap<>();
     private static Map<String, Integer> countryDigitLimits = new HashMap<>();
 
     static {
-        
+        // Country codes and their corresponding names
         countryCodes.put("1", "USA/Canada");
         countryCodes.put("7", "Russia/Kazakhstan");
         countryCodes.put("20", "Egypt");
@@ -117,152 +116,154 @@ public class PhoneNumberGuesser1 {
         countryCodes.put("298", "Faroe Islands");
         countryCodes.put("299", "Greenland");
 
-        countryDigitLimits.put("1", 10);  // USA/Canada
-        countryDigitLimits.put("7", 10);  // Russia/Kazakhstan
-        countryDigitLimits.put("20", 10); // Egypt
-        countryDigitLimits.put("27", 9);  // South Africa
-        countryDigitLimits.put("30", 10); // Greece
-        countryDigitLimits.put("31", 9);  // Netherlands
-        countryDigitLimits.put("32", 9);  // Belgium
-        countryDigitLimits.put("33", 9);  // France
-        countryDigitLimits.put("34", 9);  // Spain
-        countryDigitLimits.put("36", 9);  // Hungary
-        countryDigitLimits.put("39", 10); // Italy
-        countryDigitLimits.put("40", 10); // Romania
-        countryDigitLimits.put("41", 9);  // Switzerland
-        countryDigitLimits.put("43", 9);  // Austria
-        countryDigitLimits.put("44", 10); // United Kingdom
-        countryDigitLimits.put("45", 8);  // Denmark
-        countryDigitLimits.put("46", 9);  // Sweden
-        countryDigitLimits.put("47", 8);  // Norway
-        countryDigitLimits.put("48", 9);  // Poland
-        countryDigitLimits.put("49", 10); // Germany
-        countryDigitLimits.put("51", 9);  // Peru
-        countryDigitLimits.put("52", 10); // Mexico
-        countryDigitLimits.put("53", 8);  // Cuba
-        countryDigitLimits.put("54", 10); // Argentina
-        countryDigitLimits.put("55", 11); // Brazil
-        countryDigitLimits.put("56", 9);  // Chile
-        countryDigitLimits.put("57", 10); // Colombia
-        countryDigitLimits.put("58", 10); // Venezuela
-        countryDigitLimits.put("60", 10); // Malaysia
-        countryDigitLimits.put("61", 9);  // Australia
-        countryDigitLimits.put("62", 11); // Indonesia
-        countryDigitLimits.put("63", 10); // Philippines
-        countryDigitLimits.put("64", 9);  // New Zealand
-        countryDigitLimits.put("65", 8);  // Singapore
-        countryDigitLimits.put("66", 9);  // Thailand
-        countryDigitLimits.put("81", 10); // Japan
-        countryDigitLimits.put("82", 10); // South Korea
-        countryDigitLimits.put("84", 10); // Vietnam
-        countryDigitLimits.put("86", 11); // China
-        countryDigitLimits.put("90", 10); // Turkey
-        countryDigitLimits.put("91", 10); // India
-        countryDigitLimits.put("92", 10); // Pakistan
-        countryDigitLimits.put("93", 9);  // Afghanistan
-        countryDigitLimits.put("94", 10); // Sri Lanka
-        countryDigitLimits.put("95", 9);  // Myanmar
-        countryDigitLimits.put("98", 10); // Iran
-        countryDigitLimits.put("211", 9); // South Sudan
-        countryDigitLimits.put("212", 9); // Morocco
-        countryDigitLimits.put("213", 9); // Algeria
-        countryDigitLimits.put("216", 8); // Tunisia
-        countryDigitLimits.put("218", 9); // Libya
-        countryDigitLimits.put("220", 8); // Gambia
-        countryDigitLimits.put("221", 8); // Senegal
-        countryDigitLimits.put("222", 8); // Mauritania
-        countryDigitLimits.put("223", 8); // Mali
-        countryDigitLimits.put("224", 8); // Guinea
-        countryDigitLimits.put("225", 8); // Ivory Coast
-        countryDigitLimits.put("226", 8); // Burkina Faso
-        countryDigitLimits.put("227", 8); // Niger
-        countryDigitLimits.put("228", 8); // Togo
-        countryDigitLimits.put("229", 8); // Benin
-        countryDigitLimits.put("230", 8); // Mauritius
-        countryDigitLimits.put("231", 8); // Liberia
-        countryDigitLimits.put("232", 8); // Sierra Leone
-        countryDigitLimits.put("233", 9); // Ghana
-        countryDigitLimits.put("234", 10); // Nigeria
-        countryDigitLimits.put("235", 8); // Chad
-        countryDigitLimits.put("236", 8); // Central African Republic
-        countryDigitLimits.put("237", 9); // Cameroon
-        countryDigitLimits.put("238", 7); // Cape Verde
-        countryDigitLimits.put("239", 8); // Sao Tome and Principe
-        countryDigitLimits.put("240", 9); // Equatorial Guinea
-        countryDigitLimits.put("241", 8); // Gabon
-        countryDigitLimits.put("242", 9); // Republic of the Congo
-        countryDigitLimits.put("243", 9); // Democratic Republic of the Congo
-        countryDigitLimits.put("244", 9); // Angola
-        countryDigitLimits.put("245", 8); // Guinea-Bissau
-        countryDigitLimits.put("246", 7); // British Indian Ocean Territory
-        countryDigitLimits.put("248", 7); // Seychelles
-        countryDigitLimits.put("249", 8); // Sudan
-        countryDigitLimits.put("250", 9); // Rwanda
-        countryDigitLimits.put("251", 9); // Ethiopia
-        countryDigitLimits.put("252", 9); // Somalia
-        countryDigitLimits.put("253", 9); // Djibouti
-        countryDigitLimits.put("254", 9); // Kenya
-        countryDigitLimits.put("255", 9); // Tanzania
-        countryDigitLimits.put("256", 9); // Uganda
-        countryDigitLimits.put("257", 7); // Burundi
-        countryDigitLimits.put("258", 9); // Mozambique
-        countryDigitLimits.put("260", 9); // Zambia
-        countryDigitLimits.put("261", 9); // Madagascar
-        countryDigitLimits.put("262", 10); // Reunion
-        countryDigitLimits.put("263", 9); // Zimbabwe
-        countryDigitLimits.put("264", 9); // Namibia
-        countryDigitLimits.put("265", 9); // Malawi
-        countryDigitLimits.put("266", 8); // Lesotho
-        countryDigitLimits.put("267", 7); // Botswana
-        countryDigitLimits.put("268", 8); // Eswatini
-        countryDigitLimits.put("269", 7); // Comoros
-        countryDigitLimits.put("290", 3); // Saint Helena
-        countryDigitLimits.put("291", 7); // Eritrea
-        countryDigitLimits.put("297", 7); // Aruba
-        countryDigitLimits.put("298", 7); // Faroe Islands
-        countryDigitLimits.put("299", 7); // Greenland
+        // Expected lengths including dialing codes
+        countryDigitLimits.put("1", 11);   // USA/Canada (1 + 10 digits)
+        countryDigitLimits.put("7", 11);   // Russia/Kazakhstan (7 + 10 digits)
+        countryDigitLimits.put("20", 11);  // Egypt (20 + 9 digits)
+        countryDigitLimits.put("27", 11);  // South Africa (27 + 9 digits)
+        countryDigitLimits.put("30", 11);  // Greece (30 + 10 digits)
+        countryDigitLimits.put("31", 10);   // Netherlands (31 + 9 digits)
+        countryDigitLimits.put("32", 9);    // Belgium (32 + 8 digits)
+        countryDigitLimits.put("33", 9);    // France (33 + 8 digits)
+        countryDigitLimits.put("34", 9);    // Spain (34 + 8 digits)
+        countryDigitLimits.put("36", 9);    // Hungary (36 + 8 digits)
+        countryDigitLimits.put("39", 10);   // Italy (39 + 9 digits)
+        countryDigitLimits.put("40", 10);   // Romania (40 + 9 digits)
+        countryDigitLimits.put("41", 9);    // Switzerland (41 + 8 digits)
+        countryDigitLimits.put("43", 9);    // Austria (43 + 8 digits)
+        countryDigitLimits.put("44", 10);   // United Kingdom (44 + 9 digits)
+        countryDigitLimits.put("45", 8);    // Denmark (45 + 7 digits)
+        countryDigitLimits.put("46", 9);    // Sweden (46 + 8 digits)
+        countryDigitLimits.put("47", 8);    // Norway (47 + 7 digits)
+        countryDigitLimits.put("48", 9);    // Poland (48 + 8 digits)
+        countryDigitLimits.put("49", 10);   // Germany (49 + 9 digits)
+        countryDigitLimits.put("51", 9);    // Peru (51 + 8 digits)
+        countryDigitLimits.put("52", 10);   // Mexico (52 + 10 digits)
+        countryDigitLimits.put("53", 8);    // Cuba (53 + 7 digits)
+        countryDigitLimits.put("54", 10);   // Argentina (54 + 10 digits)
+        countryDigitLimits.put("55", 11);   // Brazil (55 + 10 digits)
+        countryDigitLimits.put("56", 9);    // Chile (56 + 8 digits)
+        countryDigitLimits.put("57", 10);   // Colombia (57 + 10 digits)
+        countryDigitLimits.put("58", 10);   // Venezuela (58 + 10 digits)
+        countryDigitLimits.put("60", 10);   // Malaysia (60 + 9 digits)
+        countryDigitLimits.put("61", 9);    // Australia (61 + 8 digits)
+        countryDigitLimits.put("62", 11);   // Indonesia (62 + 10 digits)
+        countryDigitLimits.put("63", 10);   // Philippines (63 + 9 digits)
+        countryDigitLimits.put("64", 9);    // New Zealand (64 + 8 digits)
+        countryDigitLimits.put("65", 8);    // Singapore (65 + 7 digits)
+        countryDigitLimits.put("66", 9);    // Thailand (66 + 8 digits)
+        countryDigitLimits.put("81", 11);   // Japan (81 + 10 digits)
+        countryDigitLimits.put("82", 11);   // South Korea (82 + 10 digits)
+        countryDigitLimits.put("84", 11);   // Vietnam (84 + 10 digits)
+        countryDigitLimits.put("86", 11);   // China (86 + 10 digits)
+        countryDigitLimits.put("90", 10);   // Turkey (90 + 9 digits)
+        countryDigitLimits.put("91", 12);   // India (91 + 10 digits)
+        countryDigitLimits.put("92", 11);   // Pakistan (92 + 10 digits)
+        countryDigitLimits.put("93", 11);   // Afghanistan (93 + 10 digits)
+        countryDigitLimits.put("94", 11);   // Sri Lanka (94 + 10 digits)
+        countryDigitLimits.put("95", 11);   // Myanmar (95 + 10 digits)
+        countryDigitLimits.put("98", 11);   // Iran (98 + 10 digits)
+        countryDigitLimits.put("211", 13);  // South Sudan (211 + 12 digits)
+        countryDigitLimits.put("212", 9);    // Morocco (212 + 8 digits)
+        countryDigitLimits.put("213", 9);    // Algeria (213 + 8 digits)
+        countryDigitLimits.put("216", 8);    // Tunisia (216 + 7 digits)
+        countryDigitLimits.put("218", 9);    // Libya (218 + 8 digits)
+        countryDigitLimits.put("220", 9);    // Gambia (220 + 8 digits)
+        countryDigitLimits.put("221", 9);    // Senegal (221 + 8 digits)
+        countryDigitLimits.put("222", 9);    // Mauritania (222 + 8 digits)
+        countryDigitLimits.put("223", 9);    // Mali (223 + 8 digits)
+        countryDigitLimits.put("224", 9);    // Guinea (224 + 8 digits)
+        countryDigitLimits.put("225", 10);   // Ivory Coast (225 + 9 digits)
+        countryDigitLimits.put("226", 10);   // Burkina Faso (226 + 9 digits)
+        countryDigitLimits.put("227", 9);    // Niger (227 + 8 digits)
+        countryDigitLimits.put("228", 8);    // Togo (228 + 7 digits)
+        countryDigitLimits.put("229", 8);    // Benin (229 + 7 digits)
+        countryDigitLimits.put("230", 9);    // Mauritius (230 + 8 digits)
+        countryDigitLimits.put("231", 8);    // Liberia (231 + 7 digits)
+        countryDigitLimits.put("232", 9);    // Sierra Leone (232 + 8 digits)
+        countryDigitLimits.put("233", 10);   // Ghana (233 + 9 digits)
+        countryDigitLimits.put("234", 11);   // Nigeria (234 + 10 digits)
+        countryDigitLimits.put("235", 11);   // Chad (235 + 10 digits)
+        countryDigitLimits.put("236", 10);   // Central African Republic (236 + 9 digits)
+        countryDigitLimits.put("237", 10);   // Cameroon (237 + 9 digits)
+        countryDigitLimits.put("238", 9);    // Cape Verde (238 + 8 digits)
+        countryDigitLimits.put("239", 9);    // Sao Tome and Principe (239 + 8 digits)
+        countryDigitLimits.put("240", 10);   // Equatorial Guinea (240 + 9 digits)
+        countryDigitLimits.put("241", 9);    // Gabon (241 + 8 digits)
+        countryDigitLimits.put("242", 11);   // Republic of the Congo (242 + 10 digits)
+        countryDigitLimits.put("243", 12);   // Democratic Republic of the Congo (243 + 11 digits)
+        countryDigitLimits.put("244", 10);   // Angola (244 + 9 digits)
+        countryDigitLimits.put("245", 9);    // Guinea-Bissau (245 + 8 digits)
+        countryDigitLimits.put("246", 10);   // British Indian Ocean Territory (246 + 9 digits)
+        countryDigitLimits.put("248", 9);    // Seychelles (248 + 8 digits)
+        countryDigitLimits.put("249", 10);   // Sudan (249 + 9 digits)
+        countryDigitLimits.put("250", 10);   // Rwanda (250 + 9 digits)
+        countryDigitLimits.put("251", 10);   // Ethiopia (251 + 9 digits)
+        countryDigitLimits.put("252", 9);    // Somalia (252 + 8 digits)
+        countryDigitLimits.put("253", 9);    // Djibouti (253 + 8 digits)
+        countryDigitLimits.put("254", 10);   // Kenya (254 + 9 digits)
+        countryDigitLimits.put("255", 10);   // Tanzania (255 + 9 digits)
+        countryDigitLimits.put("256", 10);   // Uganda (256 + 9 digits)
+        countryDigitLimits.put("257", 9);    // Burundi (257 + 8 digits)
+        countryDigitLimits.put("258", 10);   // Mozambique (258 + 9 digits)
+        countryDigitLimits.put("260", 10);   // Zambia (260 + 9 digits)
+        countryDigitLimits.put("261", 10);   // Madagascar (261 + 9 digits)
+        countryDigitLimits.put("262", 10);   // Reunion (262 + 9 digits)
+        countryDigitLimits.put("263", 10);   // Zimbabwe (263 + 9 digits)
+        countryDigitLimits.put("264", 10);   // Namibia (264 + 9 digits)
+        countryDigitLimits.put("265", 9);    // Malawi (265 + 8 digits)
+        countryDigitLimits.put("266", 9);    // Lesotho (266 + 8 digits)
+        countryDigitLimits.put("267", 10);   // Botswana (267 + 9 digits)
+        countryDigitLimits.put("268", 9);    // Eswatini (268 + 8 digits)
+        countryDigitLimits.put("269", 9);    // Comoros (269 + 8 digits)
+        countryDigitLimits.put("290", 9);    // Saint Helena (290 + 8 digits)
+        countryDigitLimits.put("291", 9);    // Eritrea (291 + 8 digits)
+        countryDigitLimits.put("297", 10);   // Aruba (297 + 9 digits)
+        countryDigitLimits.put("298", 10);   // Faroe Islands (298 + 9 digits)
+        countryDigitLimits.put("299", 10);   // Greenland (299 + 9 digits);
     }
 
     public static String guessCountry(String phoneNumber) {
         String cleanedNumber = phoneNumber.replaceAll("[^0-9]", "");
-        
+
         for (String code : countryCodes.keySet()) {
             if (cleanedNumber.startsWith(code)) {
                 return countryCodes.get(code);
             }
         }
-        
+
         return "Unknown Country";
     }
 
     public static String validatePhoneNumberLength(String phoneNumber) {
         String cleanedNumber = phoneNumber.replaceAll("[^0-9]", "");
-        
+
         for (Map.Entry<String, Integer> entry : countryDigitLimits.entrySet()) {
             String code = entry.getKey();
-            int expectedLength = entry.getValue();
-            
+            int expectedTotalLength = entry.getValue();
+
+            // Check if the cleaned number starts with the country code
             if (cleanedNumber.startsWith(code)) {
-                if (cleanedNumber.length() == expectedLength) {
+                if (cleanedNumber.length() == expectedTotalLength) {
                     return "The phone number is valid.";
                 } else {
-                    return "The phone number is invalid. Expected length: " + expectedLength;
+                    return "The phone number is invalid. Expected total length (including dialing code): " + expectedTotalLength;
                 }
             }
         }
-        
+
         return "Unknown country code.";
     }
 
     private static void createAndShowGUI() {
         JFrame frame = new JFrame("Phone Number Guesser");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(400, 300);
-        
+        frame.setSize(400, 400);  // Increased height for better layout
+
         JPanel panel = new JPanel();
         frame.add(panel);
         placeComponents(panel);
-        
+
         frame.setVisible(true);
     }
 
@@ -286,15 +287,19 @@ public class PhoneNumberGuesser1 {
         panel.add(validateButton);
 
         JTextArea resultArea = new JTextArea();
-        resultArea.setBounds(10, 120, 340, 100);
-        panel.add(resultArea);
+        resultArea.setLineWrap(true);
+        resultArea.setWrapStyleWord(true);
+
+        JScrollPane scrollPane = new JScrollPane(resultArea);
+        scrollPane.setBounds(10, 120, 340, 150);
+        panel.add(scrollPane);
 
         guessButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String inputNumber = userText.getText();
-                String country = guessCountry(inputNumber);
-                resultArea.setText("Country: " + country);
+                String result = guessCountry(inputNumber);
+                resultArea.setText(result);
             }
         });
 
@@ -302,18 +307,14 @@ public class PhoneNumberGuesser1 {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String inputNumber = userText.getText();
-                String validationMessage = validatePhoneNumberLength(inputNumber);
-                resultArea.setText(validationMessage);
+                String validationResult = validatePhoneNumberLength(inputNumber);
+                resultArea.setText(validationResult);
             }
         });
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> createAndShowGUI());
+        javax.swing.SwingUtilities.invokeLater(() -> createAndShowGUI());
     }
 }
-
-
-
-
 
